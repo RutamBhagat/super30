e1dc4121a5f481526a1300108309f0daa213e0e2
RutamBhagat <rutambhagat@gmail.com>
2024-10-16 03:54:49 -0700

    cleanup

--- project-1-probo/src/seed/seed.ts
+++ project-1-probo/src/seed/seed.ts
@@ -1,21 +1,16 @@
 import type { NewUser } from '@/schema/user';
 import process from 'node:process';
-import { addUrl, addVisit } from '@/services/url-services';
 import { addUser } from '@/services/user-services';
 import { faker } from '@faker-js/faker';
 import consola from 'consola';
 
 const NUM_USERS = 10;
-const MAX_URLS_PER_USER = 5;
-const MAX_VISITS_PER_URL = 10;
 
 function generateUser(): NewUser {
   return {
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
     email: faker.internet.email(),
-    gender: faker.helpers.arrayElement(['MALE', 'FEMALE']),
-    jobTitle: faker.person.jobTitle(),
     password: faker.internet.password(),
   };
 }
@@ -28,33 +23,7 @@ async function seedDatabase() {
     consola.info(`Creating user: ${userData.email}`);
 
     try {
-      const { user: newUser } = await addUser(userData);
-
-      const numUrls = faker.number.int({ min: 1, max: MAX_URLS_PER_USER });
-      for (let j = 0; j < numUrls; j++) {
-        consola.info(`Creating URL for user: ${newUser.email}`);
-
-        try {
-          const { newURL } = await addUrl({
-            redirectURL: faker.internet.url(),
-          }, newUser.id);
-
-          const numVisits = faker.number.int({ min: 1, max: MAX_VISITS_PER_URL });
-          for (let k = 0; k < numVisits; k++) {
-            consola.info(`Creating visit history for URL: ${newURL.shortID}`);
-
-            try {
-              await addVisit({ urlId: newURL.id });
-            }
-            catch (error) {
-              consola.error(`Error creating visit history: ${error}`);
-            }
-          }
-        }
-        catch (error) {
-          consola.error(`Error creating URL: ${error}`);
-        }
-      }
+      await addUser(userData);
     }
     catch (error) {
       consola.error(`Error creating user: ${error}`);
